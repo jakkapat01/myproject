64f12759129548c421a746fb132e2a600bbe923d
jakkapat.pan@kkumail.com <jakkapat.pan@kkumail.com>
2025-02-12 00:12:50 +0700

    stat sigin,sigup,database

--- condb.php
+++ condb.php
@@ -1,6 +1,6 @@
 <?php 
 //connect database
-$condb= mysqli_connect("localhost","root","254810","workshop_booking_table") or die("Error: " . mysqli_error($condb));
+$condb= mysqli_connect("localhost","root","254810","workshop_booking_table") or die("Error: " . mysqli_connect_error());
 mysqli_query($condb, "SET NAMES 'utf8' ");
 date_default_timezone_set('Asia/Bangkok');
 ?>
