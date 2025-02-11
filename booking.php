e3d0f86ac5393a2097dc5353b256a5e8f98a6f9b
jakkapat.pan@kkumail.com <jakkapat.pan@kkumail.com>
2025-02-11 19:32:54 +0700

    update

--- booking.php
+++ booking.php
@@ -22,76 +22,41 @@ $row = mysqli_fetch_array($result);
     }
     </style>
   </head>
-  <body style="background-color: #330000;">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-2 col-md-2"></div>
-        <div class="col-12 col-sm-11 col-md-7 devbanban" style="margin-top: 50px;">
-          <br>
-          <h4 align="center" style="color: red;">แสดงรายชื่อโต๊ะจากฐานข้อมูล PHP+MySQLi+Bootstrap4</h4>
-          <br>
-          <div class="row">
-            <div class="col-sm-12 col-md-12">
-              <div class="alert alert-warning" role="alert">
-                <center><font color="red"> <b> บันทึกการเลือกโต๊ะ *ให้พนักงานเลือกให้ เลือกและจองวันต่อวัน </b></font> </center>
-              </div>
-              <hr>
-                <div style="margin-left: 20px;">
-                  <form action="save_booking.php" method="post">
-                    <div class="form-group row">
-                      <label class="col-sm-2 ">เลขโต๊ะ</label>
-                      <div class="col-sm-4">
-                        <input type="text" name="table_name" class="form-control" disabled value="<?php echo $row['table_name'];?>">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label class="col-sm-2 ">ผู้จอง</label>
-                      <div class="col-sm-7">
-                        <input type="text" name="booking_name" class="form-control" required placeholder="ชื่อผู้จอง" minlength="5">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label class="col-sm-2 ">วันที่</label>
-                      <div class="col-sm-5">
-                        <input type="date" name="booking_date" class="form-control" required value="<?php echo date('Y-m-d');?>" min="<?php echo date('Y-m-d');?>" max="<?php echo date('Y-m-d');?>">
-                      </div>
-                      <label class="col-sm-1 ">เวลา</label>
-                      <div class="col-sm-3">
-                        <input type="time" name="booking_time" class="form-control" required placeholder="เวลา">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label class="col-sm-2 ">เบอร์โทร</label>
-                      <div class="col-sm-7">
-                        <input type="text" name="booking_phone" class="form-control" required placeholder="เบอร์โทร" minlength="10" maxlength="10">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label class="col-sm-2 ">ผู้บันทึก</label>
-                      <div class="col-sm-3">
-                        <input type="text" name="booking_staff" class="form-control" readonly value="พนง.">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label class="col-sm-2 "></label>
-                      <div class="col-sm-10">
-                        <input type="hidden" name="table_id" value="<?php echo $_GET['id'];?>">
-                       <button type="submit" class="btn btn-success">บันทึกการจอง</button>
-                       <br>
-                       ศึกษาการทำระบบ เพิ่ม ลบ แก้ไข แสดงข้อมูล เพิ่มเติมได้ที่ :
-                       <a href="https://www.youtube.com/playlist?list=PLEA4F1w-xYVadrcnedOqksEoOhh4gZfzO" target="_blank">
-                        Click
-                      </a>
-                      </div>
-                    </div>
-                  </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  <body style="background-color:black;">
+  <div class="container d-flex justify-content-center align-items-center min-vh-100 mt-5">
+    <div class="col-12 col-sm-11 col-md-7 devbanban bg-white p-4 rounded">
+      <h4 class="text-center text-danger">กรอกข้อมูล</h4>
+      <div class="alert alert-warning text-center" role="alert">
+        <b class="text-danger"> กรอกข้อมูลให้ตรงตามความจริง </b>
+      </div>
+      <form action="save_booking.php" method="post">
+        <div class="form-group">
+          <label>เลขโต๊ะ</label>
+          <input type="text" class="form-control" disabled value="<?php echo $row['table_name']; ?>">
         </div>
-        <hr>
-        <center> <font color="white"> designed by devbanban.com  สนับสนุนได้ที่ <a href="https://devbanban.com/?page_id=2309" target="_blank"> คลิก </a></font></center>
-      </body>
+        <div class="form-group">
+          <label>ผู้จอง</label>
+          <input type="text" name="booking_name" class="form-control" required placeholder="ชื่อผู้จอง" minlength="5">
+        </div>
+        <div class="form-group">
+          <label>วันที่</label>
+          <input type="date" name="booking_date" class="form-control" required value="<?php echo date('Y-m-d'); ?>" min="<?php echo date('Y-m-d'); ?>">
+        </div>
+        <div class="form-group">
+          <label>เวลา</label>
+          <input type="time" name="booking_time" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label>เบอร์โทร</label>
+          <input type="text" name="booking_phone" class="form-control" required placeholder="เบอร์โทร" minlength="10" maxlength="10">
+        </div>
+        <input type="hidden" name="table_id" value="<?php echo $_GET['id']; ?>">
+        <button type="submit" class="btn btn-success btn-block">บันทึกการจอง</button>
+      </form>
+    </div>
+  </div>
+  <footer class="text-center text-white mt-3">
+    สนับสนุนได้ที่ <a href="https://www.instagram.com/jatokapoa?igsh=MTRrc2ljc3VxcWd6" target="_blank" class="text-info">คลิก</a>
+  </footer>
+</body>
     </html>
---
+++ booktable.php
@@ -0,0 +1,53 @@
+<?php
+//เรียกใช้งานไฟล์เชื่อมต่อฐานข้อมูล
+require_once 'condb.php';
+//query
+$query = "SELECT * FROM tbl_table ORDER BY id ASC";
+$result = mysqli_query($condb, $query);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>จองโต๊ะ</title>
+    <style type="text/css">
+    .devbanban{
+    background-color: #ffffff;
+    }
+    </style>
+  </head>
+  <body style="background-color: #000000;">
+  <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="col-12 col-sm-11 col-md-7 devbanban p-4">
+      <h4 class="text-center text-danger">เลือกโต๊ะ</h4>
+      <div class="alert alert-warning text-center" role="alert">
+        Tables
+      </div>
+      <hr>
+      <div class="row justify-content-center">
+        <?php foreach ($result as $row) {
+          if ($row['table_status'] == 0) { // ว่าง
+            echo '<div class="col-2 m-1">';
+            echo '<a href="booking.php?id='.$row["id"].'&act=booking" class="btn btn-success">'.$row['table_name'].'</a></div>';
+          } else { // ถูกจอง
+            echo '<div class="col-2 m-1">';
+            echo '<a href="#" class="btn btn-secondary disabled">'.$row['table_name'].'</a></div>';
+          }
+        } ?>
+      </div>
+      <p class="text-center" style ="padding-top: 30px;" >*เขียว = ว่าง, เทา = ไม่ว่าง</p>
+    </div>
+  </div>
+  <hr>
+  <center> 
+    <font color="white"> สนับสนุนได้ที่ 
+      <a href="https://www.instagram.com/jatokapoa?igsh=MTRrc2ljc3VxcWd6" target="_blank"> คลิก </a>
+    </font>
+  </center>
+</body>
+
+    </html>
