5b2ed430d9239fb1769a1119b6adf67d944d2275
jakkapat.pan@kkumail.com <jakkapat.pan@kkumail.com>
2025-02-15 04:44:52 +0700

    before payment

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51QsUExKGHM8QkGaDgkh3ulst8SjgQW4mQtd55yy6sjmSwmchYp8poYGuGLQxrN9mAhCh7zxsI9tSxhruKtXXFqTQ00pj8qLbBn';
